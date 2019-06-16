0264b2c7169e06f67e98972facd3e36a1da6c7b3
Wiktor Balcerzak <wbalcerzak97@o2.pl>
2019-06-17 00:13:38 +0200

    Writeline is working

--- mw/Program.cs
+++ mw/Program.cs
@@ -6,7 +6,31 @@ namespace mw
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime d1;
+            d1 = DateTime.Parse(Console.ReadLine());
+
+            DateTime d2;
+            d2 = DateTime.Parse(Console.ReadLine());
+
+
+            if (d1.Month == d2.Month && d1.Year == d2.Year)
+            {
+                Console.WriteLine(d1.Day.ToString("00") + " - " + d2.ToString("d"));
+            }
+
+            else if (d1.Year == d2.Year)
+            {
+                Console.WriteLine(d1.Date.ToString("dd.MM") + " - " + d2.ToString("d"));
+            }
+
+            else if(d1.Month == d2.Month)
+            {
+                Console.WriteLine(d1.ToString("d") + " - " + d2.ToString("d"));
+                Console.WriteLine("Tylko ten sam miesiac");
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
